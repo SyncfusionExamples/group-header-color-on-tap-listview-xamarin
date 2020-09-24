8f4da94be9b6d982556805934f8911c827f776bb
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-09-24 12:56:30 +0530

    XAMARIN-37162 - Sample and screenshot added.

---
+++ ListViewXamarin/ListViewXamarin/ListViewXamarin/Helper/Behavior.cs
@@ -0,0 +1,47 @@
+﻿using Syncfusion.DataSource.Extensions;
+using Syncfusion.GridCommon.ScrollAxis;
+using Syncfusion.ListView.XForms;
+using Syncfusion.ListView.XForms.Control.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListViewXamarin
+{
+    public class Behavior : Behavior<ContentPage>
+    {
+        SfListView listView;
+        protected override void OnAttachedTo(ContentPage bindable)
+        {
+            listView = bindable.FindByName<SfListView>("listView");
+            listView.ItemTapped += ListView_ItemTapped;
+            base.OnAttachedTo(bindable);
+        }
+
+        private void ListView_ItemTapped(object sender, Syncfusion.ListView.XForms.ItemTappedEventArgs e)
+        {
+            var groupResult = e.ItemData as GroupResult;
+            if (e.ItemType is ItemType.GroupHeader)
+            {
+                foreach (var item in groupResult.Items)
+                {
+                    if (!(item as Contacts).IsHeaderTapped)
+                        (item as Contacts).IsHeaderTapped = true;
+                    else
+                        (item as Contacts).IsHeaderTapped = false;
+                }
+                listView.RefreshListViewItem(-1, -1, true);
+            }
+        }
+        protected override void OnDetachingFrom(BindableObject bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            listView.ItemTapped -= ListView_ItemTapped;
+        }
+    }
+}
---
+++ ListViewXamarin/ListViewXamarin/ListViewXamarin/Helper/GroupHeaderConverter.cs
@@ -0,0 +1,28 @@
+﻿using Syncfusion.DataSource.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ListViewXamarin
+{
+    public class GroupHeaderConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return Color.Yellow;
+            var items = (value as GroupResult).Items.ToList<Contacts>().ToList();
+            if (items[0].IsHeaderTapped)
+                return Color.Green;
+            else
+                return Color.Yellow;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
